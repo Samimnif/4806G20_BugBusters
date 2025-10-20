498973477c1625714ded508ae65895db7ba82140
Sparsh Paliya <paliyasparsh@gmail.com>
2025-10-20 17:24:11 +0200

    Optimize application performance and resource handling

--- src/main/java/org/sysc4806/sysc4806_group20/Repository/TopicRepository.java
+++ src/main/java/org/sysc4806/sysc4806_group20/Repository/TopicRepository.java
@@ -1,7 +1,20 @@
 package org.sysc4806.sysc4806_group20.Repository;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.sysc4806.sysc4806_group20.Model.Topic;
 
-public interface TopicRepository extends CrudRepository<Topic, Long> {
+public interface TopicRepository extends JpaRepository<Topic, Long> {
+	Page<Topic> findAll(Pageable pageable);
+
+    @EntityGraph(attributePaths = { "submissions", "professor" })
+    @Query("select t from Topic t where t.id = :id")
+    Optional<Topic> findByIdWithDetails(@Param("id") Long id);
 }
+
